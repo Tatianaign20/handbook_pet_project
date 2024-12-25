f3c83f55684dc326d983e8c65a03f0fd819aeb5c
Tania Ryabets <tatianaign20@yandex.ru>
2024-12-25 14:41:01 +0300

    Подправила, запустила  run lint -- --fix

--- src/services/slices/cardSlice.test.tsx
+++ src/services/slices/cardSlice.test.tsx
@@ -12,7 +12,6 @@ import {
 import { getCardsApi, addCardApi, delCardApi } from 'src/utils/handbook-api';
 import store from '../store';
 
-
 const cardSliceReducer = cardsSlice.reducer;
 
 const testCard = {
@@ -54,11 +53,12 @@ const mockData = [
   }
 ];
 
-
 jest.mock('../../utils/handbook-api', () => ({
   getCardsApi: jest.fn(() => Promise.resolve(mockData)),
   addCardApi: jest.fn(() => Promise.resolve(testCard)),
-  delCardApi: jest.fn(() => Promise.resolve({menuItemId:testCard.menuItemId, id: testCard.id})),
+  delCardApi: jest.fn(() =>
+    Promise.resolve({ menuItemId: testCard.menuItemId, id: testCard.id })
+  )
   //changeCardTextApi: jest.fn() => Promise....),
   // changeCardImageApi: jest.fn()=> Promise....),
 }));
@@ -72,30 +72,26 @@ afterEach(() => {
 const mockaddCardApi = require('../../utils/handbook-api')
   .addCardApi as jest.MockedFunction<typeof addCardApi>;
 
-  afterEach(() => {
-    mockaddCardApi.mockClear();
-  });
+afterEach(() => {
+  mockaddCardApi.mockClear();
+});
 
-  const mockdelCardApi = require('../../utils/handbook-api')
+const mockdelCardApi = require('../../utils/handbook-api')
   .delCardApi as jest.MockedFunction<typeof delCardApi>;
 
-  afterEach(() => {
-    mockdelCardApi.mockClear();
-  });
+afterEach(() => {
+  mockdelCardApi.mockClear();
+});
 
 describe('проверка getCards rejected', () => {
-
   it('Проверка getCards rejected', async () => {
-
     const mockError = {
       name: 'MockError',
       message: 'Mock Error Message',
       stack: 'Mock Stack Trace'
     };
     try {
-      await store.dispatch(
-        getCards.rejected(mockError, '', 1)
-      );
+      await store.dispatch(getCards.rejected(mockError, '', 1));
     } catch (error) {}
 
     expect(store.getState().cardReducer).toEqual({
@@ -107,18 +103,14 @@ describe('проверка getCards rejected', () => {
 });
 
 describe('проверка addCard rejected', () => {
-
   it('Проверка addCard rejected', async () => {
-
     const mockError = {
       name: 'MockError',
       message: 'Mock Error Message',
       stack: 'Mock Stack Trace'
     };
     try {
-      await store.dispatch(
-        addCard.rejected(mockError, '', testCard)
-      );
+      await store.dispatch(addCard.rejected(mockError, '', testCard));
     } catch (error) {}
 
     expect(store.getState().cardReducer).toEqual({
@@ -130,9 +122,7 @@ describe('проверка addCard rejected', () => {
 });
 
 describe('проверка delCard rejected', () => {
-
   it('Проверка delCard rejected', async () => {
-
     const mockError = {
       name: 'MockError',
       message: 'Mock Error Message',
@@ -140,7 +130,10 @@ describe('проверка delCard rejected', () => {
     };
     try {
       await store.dispatch(
-        delCard.rejected(mockError, '', { menuItemId: testCard.menuItemId, id: testCard.id})
+        delCard.rejected(mockError, '', {
+          menuItemId: testCard.menuItemId,
+          id: testCard.id
+        })
       );
     } catch (error) {}
 
@@ -153,46 +146,44 @@ describe('проверка delCard rejected', () => {
 });
 
 describe('проверка getCards pending', () => {
-    it('проверка getCards  pending', async () => {
+  it('проверка getCards  pending', async () => {
     store.dispatch(getCards(1));
 
     expect(store.getState().cardReducer.isLoading).toBe(true);
     expect(store.getState().cardReducer.error).toBeNull();
   });
-
 });
 describe('проверка addCard pending', () => {
   it('проверка addCard  pending', async () => {
-  store.dispatch(addCard( testCard));
+    store.dispatch(addCard(testCard));
 
-  expect(store.getState().cardReducer.isLoading).toBe(true);
-  expect(store.getState().cardReducer.error).toBeNull();
-});
+    expect(store.getState().cardReducer.isLoading).toBe(true);
+    expect(store.getState().cardReducer.error).toBeNull();
+  });
 });
 
 describe('проверка delCard pending', () => {
   it('проверка delCard  pending', async () => {
-  store.dispatch(delCard( { menuItemId: testCard.menuItemId, id: testCard.id}));
+    store.dispatch(
+      delCard({ menuItemId: testCard.menuItemId, id: testCard.id })
+    );
 
-  expect(store.getState().cardReducer.isLoading).toBe(true);
-  expect(store.getState().cardReducer.error).toBeNull();
-
-});
+    expect(store.getState().cardReducer.isLoading).toBe(true);
+    expect(store.getState().cardReducer.error).toBeNull();
+  });
 });
 
 describe('проверка getCards fulfilled', () => {
+  it('Проверка getCards fulfilled', async () => {
+    await store.dispatch(getCards(1));
 
-it('Проверка getCards fulfilled', async () => {
-  await store.dispatch(getCards(1));
-
-  expect(store.getState().cardReducer.isLoading).toBe(false);
-  expect(store.getState().cardReducer.error).toBeNull();
-  expect(store.getState().cardReducer.data).toEqual(mockData);
-});
+    expect(store.getState().cardReducer.isLoading).toBe(false);
+    expect(store.getState().cardReducer.error).toBeNull();
+    expect(store.getState().cardReducer.data).toEqual(mockData);
+  });
 });
 
 describe('проверка addCard fulfilled', () => {
-
   it('Проверка addCard fulfilled', async () => {
     await store.dispatch(addCard(testCard));
     const mockDataAddCard = [
@@ -232,50 +223,50 @@ describe('проверка addCard fulfilled', () => {
     expect(store.getState().cardReducer.error).toBeNull();
     expect(store.getState().cardReducer.data).toEqual(mockDataAddCard);
   });
+});
+describe('проверка delCard fulfilled', () => {
+  it('Проверка delCard fulfilled', async () => {
+    await store.dispatch(
+      delCard({ menuItemId: testCard.menuItemId, id: testCard.id })
+    );
+    const mockDataDelCard = [
+      {
+        id: 11,
+        menuItemId: 1,
+        serialNumber: 1,
+        image: 'http://localhost:3001/menuitem/card/images/11.jpg',
+        text: 'text_1_1'
+      },
+      {
+        id: 12,
+        menuItemId: 1,
+        serialNumber: 1,
+        image: 'http://localhost:3001/menuitem/card/images/12.jpg',
+        text: 'text_1_222'
+      },
+      {
+        id: 13,
+        menuItemId: 1,
+        serialNumber: 1,
+        image: 'http://localhost:3001/menuitem/card/images/13.jpg',
+        text: 'text_1_3'
+      },
+      {
+        id: 14,
+        menuItemId: 1,
+        serialNumber: 1,
+        image: 'http://localhost:3001/menuitem/card/images/14.jpg',
+        text: 'text_1_4'
+      }
+      // должна удалиться тестовая карточка
+      // testCard
+    ];
+
+    expect(store.getState().cardReducer.isLoading).toBe(false);
+    expect(store.getState().cardReducer.error).toBeNull();
+    expect(store.getState().cardReducer.data).toEqual(mockDataDelCard);
   });
-  describe('проверка delCard fulfilled', () => {
-
-    it('Проверка delCard fulfilled', async () => {
-      await store.dispatch(delCard({menuItemId: testCard.menuItemId, id: testCard.id}));
-      const mockDataDelCard = [
-        {
-          id: 11,
-          menuItemId: 1,
-          serialNumber: 1,
-          image: 'http://localhost:3001/menuitem/card/images/11.jpg',
-          text: 'text_1_1'
-        },
-        {
-          id: 12,
-          menuItemId: 1,
-          serialNumber: 1,
-          image: 'http://localhost:3001/menuitem/card/images/12.jpg',
-          text: 'text_1_222'
-        },
-        {
-          id: 13,
-          menuItemId: 1,
-          serialNumber: 1,
-          image: 'http://localhost:3001/menuitem/card/images/13.jpg',
-          text: 'text_1_3'
-        },
-        {
-          id: 14,
-          menuItemId: 1,
-          serialNumber: 1,
-          image: 'http://localhost:3001/menuitem/card/images/14.jpg',
-          text: 'text_1_4'
-        },
-        // должна удалиться тестовая карточка
-        // testCard
-      ];
-  
-      expect(store.getState().cardReducer.isLoading).toBe(false);
-      expect(store.getState().cardReducer.error).toBeNull();
-      expect(store.getState().cardReducer.data).toEqual(mockDataDelCard);
-    });
-    });
-
+});
 
 describe('проверка cardSliceReducer', () => {
   let state: TCardsState;
